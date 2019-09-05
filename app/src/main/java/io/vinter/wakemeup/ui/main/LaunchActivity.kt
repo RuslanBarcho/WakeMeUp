aeb8266d994f1a8a13b72f53f4cdaac16723abb8
RuslanBarcho <RRCFoxyLoxy@yandex.ru>
2019-09-06 01:47:31 +0300

    Refactoring

--- app/src/main/java/io/vinter/wakemeup/ui/main/LaunchActivity.kt
+++ app/src/main/java/io/vinter/wakemeup/ui/main/LaunchActivity.kt
@@ -1,10 +1,10 @@
 package io.vinter.wakemeup.ui.main
 
-import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import io.vinter.wakemeup.R
+import io.vinter.wakemeup.data.preferences.PreferencesRepository
 import io.vinter.wakemeup.ui.login.LoginActivity
 
 class LaunchActivity : AppCompatActivity() {
@@ -12,8 +12,8 @@ class LaunchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launch)
-        val preferences = getSharedPreferences("userPrefs", Context.MODE_PRIVATE)
-        if (preferences.getString("token", "") == "") {
+        val preferences = PreferencesRepository(this)
+        if (preferences.getToken() == "") {
             this.startActivity(Intent(this, LoginActivity::class.java))
             this.finish()
         } else {
