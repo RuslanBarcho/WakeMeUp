cf2fbacb9dbfd95629cf82d808fc6e8ae0855143
RuslanBarcho <RRCFoxyLoxy@yandex.ru>
2019-09-12 03:36:32 +0300

    refactoring

--- app/src/main/java/io/vinter/wakemeup/App.kt
+++ app/src/main/java/io/vinter/wakemeup/App.kt
@@ -3,13 +3,14 @@ package io.vinter.wakemeup
 import android.app.Application
 import org.koin.core.context.startKoin
 import org.koin.android.ext.koin.androidContext
+import io.vinter.wakemeup.di.modules
 
 class App: Application(){
     override fun onCreate() {
         super.onCreate()
         startKoin {
             androidContext(this@App)
-            modules(io.vinter.wakemeup.di.modules)
+            modules(modules)
         }
     }
 }
